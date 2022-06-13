3923a1a5b4e46b5c13cdcdc122f271a641479795
Angelo Soares <angeelosoares@gmail.com>
2022-06-13 13:35:38 -0300

    chore: JWT package now works

--- back-end/app/Models/Admins.php
+++ back-end/app/Models/Admins.php
@@ -4,12 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class Admins extends Model
+class Admins extends Model implements JWTSubject
 {
     use HasFactory;
 
     protected $fillable = ['name', 'email', 'password'];
 
     protected $hidden = ['password'];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
