26a97845ec4be0be260a83ec38f4f52769d9c225
Angelo Soares <angeelosoares@gmail.com>
2022-06-14 18:31:38 -0300

    feat: response handler

--- back-end/app/Http/Controllers/Controller.php
+++ back-end/app/Http/Controllers/Controller.php
@@ -5,9 +5,43 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Response;
 use Illuminate\Routing\Controller as BaseController;
+
+use App\Exceptions\BadRequestException;
+use App\Exceptions\ConflictException;
+use App\Exceptions\ForbiddenException;
+use App\Exceptions\NotFoundException;
+use App\Exceptions\UnauthorizedException;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function ok($data = []): Response
+    {
+        return new Response($data);
+    }
+
+    public function error(\Exception $exception): Response
+    {
+        $errors = [];
+
+        $message = match(true){
+            $exception instanceof BadRequestException,
+                $exception instanceof UnauthorizedException,
+                $exception instanceof ConflictException,
+                $exception instanceof ForbiddenException,
+                $exception instanceof NotFoundException => $exception->getMessage(),
+            default => 'Internal Server Error'
+        };
+
+        $errors['message'] = $message;
+
+        if ($exception instanceof BadRequestException) {
+            $errors['fields'] = $exception->fields;
+        }
+
+        return new Response($errors, $exception->statusCode ?? 500);
+    }
 }
