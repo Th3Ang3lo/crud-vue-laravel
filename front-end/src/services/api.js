bda5c37e1249b302295a9ab63e0eb80828340e62
Angelo Soares <angeelosoares@gmail.com>
2022-06-15 01:30:23 -0300

    fix: authenticated axios

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -1,7 +1,12 @@
 import axios from 'axios'
 
+const token = $cookies.get('token')
+
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api'
+  baseURL: 'http://localhost:8000/api',
+  headers: {
+    Authorization: token ? `Bearer ${$cookies.get('token')}` : null
+  }
 })
 
 export default api
