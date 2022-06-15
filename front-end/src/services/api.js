c6aa6f8e51aa27c01b745688e8e157e2e743af75
Angelo Soares <angeelosoares@gmail.com>
2022-06-15 01:04:01 -0300

    feat: api

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -1,3 +1,7 @@
 import axios from 'axios'
 
-export const api = axios.create(process.env.VUE_APP_API_URL)
+const api = axios.create({
+  baseURL: 'http://localhost:8000/api'
+})
+
+export default api
